7c13fb21960b69d770c08a6d54ecde412fe0b141
marcos-guillen <10079595+marcos-guillen@users.noreply.github.com>
2023-12-04 12:22:17 +0100

    Fin del curso

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,6 +4,11 @@ import InvoiceChart from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard propio',
+};
  
 export default async function Page() {
   // const revenue = await fetchRevenue()
